e9f6c710fb55e90d34ccb4d3bbb8cc7d9436ce52
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2020-12-18 17:01:58 -0800

    Added arrays for character types

--- script.js
+++ script.js
@@ -26,6 +26,18 @@ function generatePassword() {
     alert("Password must be 128 characters or less!");
     generatePassword();
   }
+
+  var charChoice = [];
+  var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "{", "}", "~"];
+  var lowChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numChar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+  console.log(specialChar);
+  console.log(lowChar);
+  console.log(upChar);
+  console.log(numChar);
+
 }
 
 
